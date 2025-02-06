c438968add4f6e4d112597db08d53c3e84f5d700
nikk0895 <newnikk08@gmail.com>
2025-02-06 16:53:43 +0530

    Fixed the code under addTodoList and shown the target value in console

--- TodoList.js
+++ TodoList.js
@@ -1,4 +1,3 @@
-
 const mainTodoElem = document.querySelector(".todo-lists-elem"); 
 {/* //making the reference of input under mainTodoElem */}
 const inputValue = document.getElementById("inputValue"); 
@@ -9,7 +8,7 @@ const fromlocal = () => {
 };
 
 const addTodoListLocalStorage = (localTodoLists) => {
-return localStorage.setItem("initailstorageto_do",JSON.stringify(localTodoLists)
+  return localStorage.setItem("initailstorageto_do",JSON.stringify(localTodoLists)
 );
 };
 
@@ -26,54 +25,50 @@ const addTodoDynamicElement = (curElem) => {
 
 const addTodoList = (e) => {
   e.preventDefault();
-const todoListValue =inputValue.value.trim();
-
-inputValue.value = "";
-if(todoListValue != "" && !localTodoLists.includes(todoListValue))
-{
-localTodoLists.push(todoListValue);
-localTodoLists = [...new Set(localTodoLists)];
-console.log(localTodoLists);
-localStorage.setItem("initailstorageto_do",JSON.stringify(localTodoLists)
-);
-addTodoDynamicElement(todoListValue);
-}
-};
+  console.log("testing",inputValue.value);
+  if(!inputValue.value){
+    alert("please enter the name")
+  }
+  const todoListValue = inputValue.value.trim();
+  inputValue.value = "";
+  if(todoListValue != "" && !localTodoLists.includes(todoListValue))
+    {
+      localTodoLists.push(todoListValue);
+      localTodoLists = [...new Set(localTodoLists)];
+      console.log(localTodoLists);
+      localStorage.setItem("initailstorageto_do",JSON.stringify(localTodoLists)
+      );
+      addTodoDynamicElement(todoListValue);
+    }
+  };
 {/* //reusablity 
   // const divElement = document.createElement("div");
   // divElement.classList.add("main_todo_div");
   // divElement.innerHTML = `<li>${inputValue.value}</li>
   //     <button class ="deleteBtn">Delete</button> `;
   //     mainTodoElem.append(divElement) */}
-
-const showTodoList = () => {
-console.log(localTodoLists);
-
-localTodoLists.forEach((curElem) => {
-addTodoDynamicElement(curElem);
-});
-
+  const showTodoList = () => {
+  console.log(localTodoLists);
+  localTodoLists.forEach((curElem) => {
+    addTodoDynamicElement(curElem);
+  });
 };
 
 showTodoList();
 
-{/* remove the data */}
+{/*to remove data */}
 
 const removeTodoElem = (e) => {
   const todoToRemove = e.target;
   let todoListContent = todoToRemove.previousElementSibling.innerText;
   let parentElem = todoToRemove.parentElement;
   console.log(todoListContent);
-
-localTodoLists = localTodoLists.filter((curTodo) => {
-return curTodo != todoListContent.toLowerCase(); 
-});
-
-
-addTodoListLocalStorage(localTodoLists);
-parentElem.remove();
-
-console.log(localTodoLists);
+  localTodoLists = localTodoLists.filter((curTodo) => {
+  return curTodo != todoListContent.toLowerCase(); 
+  });
+  addTodoListLocalStorage(localTodoLists);
+  parentElem.remove();
+  console.log(localTodoLists);
 };
 
 mainTodoElem.addEventListener("click",(e) => {
@@ -81,7 +76,6 @@ mainTodoElem.addEventListener("click",(e) => {
   if(e.target.classList.contains("deleteBtn")){
     removeTodoElem(e);
   }
-  
 });
 
 document.querySelector(".btn").addEventListener("click", (e) => {
